a5a74efdf35f8b0cb265ef22227f994841e56c69
Olga <olga1998senko@gmail.com>
2023-03-01 00:55:11 +0300

    modified project

--- script/script.js
+++ script/script.js
@@ -1,8 +1,23 @@
 "use strict"
-let a = parseInt(prompt('First number?'));
-let b = parseInt(prompt('Second number?'));
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
+const a = parseInt(prompt('First number?'));
+const b = parseInt(prompt('Second number?'));
+function add(a, b) {
+    alert(a+b);
+}
+add(a,b);
+
+function subtract(a, b) {
+    alert(a-b);
+}
+subtract(a, b);
+
+function multiplication(a, b) {
+    alert(a*b);
+}
+multiplication(a, b);
+
+function division(a, b) {
+    alert(a/b);
+}
+division(a, b);
 
